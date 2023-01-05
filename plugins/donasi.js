85a37e5072459f736584d64f534b77d0d3e80be6
khairil <80386217+khairil8@users.noreply.github.com>
2023-01-05 21:58:34 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text, usedPrefix }) => {
 
     let img = fs.readFileSync('./thumbnail.jpg');
     let img2 = `https://telegra.ph/file/09a41ff7518f695096dab.jpg`
-    return conn.sendButton(m.chat, 'Donasi Via Shopeepay.\n❏  ', 'Makasih, thanks for you donate...', img2, [["MENU", usedPrefix + "menu"], ["OWNER", usedPrefix + "owner"]], m, {
+    return conn.sendButton(m.chat, 'Donasi Via Shopeepay. ', 'Makasih, thanks for you donate...', img2, [["MENU", usedPrefix + "menu"], ["OWNER", usedPrefix + "owner"]], m, {
         contextInfo: {
             externalAdReply: {
                 mediaUrl: "https://instagram.com/coglah_07",
